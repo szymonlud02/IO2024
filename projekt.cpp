54e41c2bfe367cabaf9acac5f8b17ab50d4c384a
Szymon Ludwikowski <szymonlud@mat.umk.pl>
2024-12-03 23:29:03 +0100

    Sprawdzanie czy liczba jest dodatnia

--- projekt.cpp
+++ projekt.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(){
 	int liczba;
 	cin >> liczba;
-	cout << liczba;
 	
+	if(liczba <=0){
+		cout << "wczytana liczba nie jest dodatnia" << endl;
+		return 0;
+	}
+	cout << liczba;
+
 	return 0;
 }
