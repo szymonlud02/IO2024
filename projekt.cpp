e86fb5a25f4fa030f4b86659df910aa99efa4721
Szymon Ludwikowski <szymonlud@mat.umk.pl>
2024-12-03 23:22:14 +0100

    Druga migawka

--- projekt.cpp
+++ projekt.cpp
@@ -2,10 +2,26 @@
 
 using namespace std;
 
+bool czy_pierwsza(int liczba){
+	if(liczba<=1) return false;
+	
+	for(int i=2; i*i<=liczba; i++){
+		if(liczba%i == 0) return false;
+	}
+	return true;
+}
+
 int main(){
 	int liczba;
 	cin >> liczba;
 	cout << liczba;
 	
+	if(czy_pierwsza(liczba)){
+		cout << " jest pierwsza" << endl;
+	}
+	else{
+		cout << " nie jest pierwsza" << endl;
+	}
+
 	return 0;
 }
